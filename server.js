dacfb03f302530f8673e0cd5e4464395c900c9e1
suhas2151 <99635833+welcomgithub@users.noreply.github.com>
2024-06-27 17:15:02 -0400

    Add files via upload
    add project

---
+++ server.js
@@ -0,0 +1,31 @@
+let http = require("http");
+let express = require("express");
+let bodyParser = require("body-parser");
+const path = require('path');
+let dbConnection = require("./Config/connection.js");
+let cors = require('cors');
+
+let app = express();
+app.use(cors());
+
+let restaurantRoutes = require('./app/routes/restaurant.routes.js');
+
+
+
+const port = 8000;
+app.use(bodyParser.json());
+
+
+// define route
+
+app.use('/restaurant', restaurantRoutes);
+app.use('/',express.static(path.join(__dirname,'public')))
+
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/', express.static(path.join(__dirname, 'angular')));
+
+app.listen(port, () => {
+    console.log(`Server Running on http://localhost:${port}`);
+})
+
